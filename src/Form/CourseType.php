516c5136ad922f58477c27a1a8f38b56bbd45629
CherrySmejkal <thanassawans@gmail.com>
2022-12-11 10:39:37 +0100

    Work on Course CRUD Layout

--- src/Form/CourseType.php
+++ src/Form/CourseType.php
@@ -4,6 +4,10 @@ namespace App\Form;
 
 use App\Entity\Course;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,14 +16,19 @@ class CourseType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('description')
-            ->add('start_date')
-            ->add('end_date')
-            ->add('price')
-            ->add('capacity')
-            ->add('available')
-            ->add('review')
-            ->add('fk_trainer_id')
+            ->add('description', TextType::class, ["attr" => ["placeholder" => "Description", "class" => "form-control m-1"]])
+            ->add('start_date', DateTimeType::class, ["attr" => ["class" => "form-control m-1"]])
+            ->add('end_date', DateTimeType::class, ["attr" => ["class" => "form-control m-1"]])
+            ->add('price', NumberType::class, ["attr" => ["class" => "form-control m-1"]])
+            ->add('capacity', NumberType::class, ["attr" => ["class" => "form-control m-1"]])
+            ->add('available', ChoiceType::class, [
+                'choices' => [
+                    'Yes' => "1",
+                    'No' => "0",
+                ], "attr" => ["class" => "form-select m-1"]
+            ])
+            // ->add('review')
+            // ->add('fk_trainer_id')
         ;
     }
 
